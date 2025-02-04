7ad1685d520ec510ed8f7ea4252bd1c5092ed39e
DAY0522 <ekdud_____@naver.com>
2025-02-04 10:53:35 +0900

    js #2

--- javascript-practices/basics/ex00.js
+++ javascript-practices/basics/ex00.js
@@ -1,1 +1,4 @@
-console.log("============ hello world");
+console.log("============ hello world1");
+console.log("============ hello world2");
+console.log("============ hello world3");
+console.log("============ hello world4");
--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,3 +1,20 @@
 console.log("============[02.02] 표현식(Expression)");
+i = 4;
+2 + 2;
+parseInt("4");
+Math.pow(2, 2);
+4;
+i; // error가 아니다!
+
 console.log("============[05] 세미콜론(Semicolon)");
+a = 10;
+b = 20;
+
 console.log("============[06] 개행(Line Break)");
+k
+= 
+10
+console.log(i)
+
+// console.lo // lo라는 속성을 찾으려고 시도함. -> error
+// g("hello world")
